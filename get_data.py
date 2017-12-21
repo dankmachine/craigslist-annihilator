e4502ff664e4539fa8b5d2710cc28012d3994c29
dankmachine <williamjow67@gmail.com>
2017-12-20 19:27:27 -0800

    fooling around...

--- get_data.py
+++ get_data.py
@@ -1,1 +1,8 @@
-# what am I doing
+import sys, os, re, os.path
+import urllib.request, urllib.error, urllib.parse
+
+def main(argv):
+    return None
+
+if __name__ == "__main__":
+    main(sys.argv)
